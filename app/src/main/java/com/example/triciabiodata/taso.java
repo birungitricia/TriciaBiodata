ef3d09fa17e0ddce6bbf636e5577d9c1e6ee93f7
elife <birungitricia1@gmail.com>
2020-02-19 16:48:58 +0300

    Android word assignment

--- app/src/main/java/com/example/triciabiodata/taso.java
+++ app/src/main/java/com/example/triciabiodata/taso.java
@@ -2,7 +2,12 @@ package com.example.triciabiodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class taso extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class taso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taso);
+        TextView textView=findViewById(R.id.btnrun);
+        AssetManager assetManager=getAssets();
+        InputStream files;
+        {
+            try {
+
+               files=assetManager.open("Android programing is good.c");
+            }catch (IOException e){
+             e.printStackTrace();
+            }
+           // textView.setText((CharSequence) files);
+
+        }
     }
 }
